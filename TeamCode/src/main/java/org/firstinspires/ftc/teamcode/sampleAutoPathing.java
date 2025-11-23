58f81ff0ab484021fa1cdae9c73ade0a24f42f90
Sanuk <sanuksamarasinghe5@gmail.com>
2025-11-23 23:17:54 +0530

    created opMode Auto test file
    Created a test for Auto Pathing with Pedro Pathing

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/sampleAutoPathing.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous
+public class sampleAutoPathing extends OpMode {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
