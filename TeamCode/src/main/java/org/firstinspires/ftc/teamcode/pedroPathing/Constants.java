c9a84815f1042a7166c07a311a958153f4fe173d
Sanuk <sanuksamarasinghe5@gmail.com>
2025-11-26 13:34:22 +0530

    Updated the Final Pedro Pathing Constants
    Did all the Pedro Pathing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -1,39 +1,62 @@
 package org.firstinspires.ftc.teamcode.pedroPathing;
 
+import com.pedropathing.control.PIDFCoefficients;
+import com.pedropathing.control.PIDFController;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.ftc.FollowerBuilder;
 import com.pedropathing.ftc.drivetrains.MecanumConstants;
 import com.pedropathing.ftc.localization.Encoder;
 import com.pedropathing.ftc.localization.constants.ThreeWheelIMUConstants;
-import com.pedropathing.ftc.localization.constants.TwoWheelConstants;
 import com.pedropathing.paths.PathConstraints;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.pedropathing.control.FilteredPIDFCoefficients;
 
 public class Constants {
     public static FollowerConstants followerConstants = new FollowerConstants()
-            .mass(5.5);
-
+            .mass(5.5)
+            .forwardZeroPowerAcceleration(-56.42879761)
+            .lateralZeroPowerAcceleration(-73.9574885591783)
+            .translationalPIDFCoefficients(new PIDFCoefficients(
+                    0.05,
+                    0,
+                    0.001,
+                    0.03
+            ))
+            .headingPIDFCoefficients(new PIDFCoefficients(
+                    0.8,
+                    0,
+                    0.03,
+                    0.028
+            ))
+            .drivePIDFCoefficients(new FilteredPIDFCoefficients(
+                    0.01,
+                    0.0,
+                    0.0002,
+                    0.0,
+                    0.03
+            ))
+            .centripetalScaling(0.025);
     public static MecanumConstants driveConstants = new MecanumConstants()
-            .maxPower(1)
+            .maxPower(1) //The Second Set
+            .leftFrontMotorName("flMotor")
+            .leftRearMotorName("blMotor")
             .rightFrontMotorName("frMotor")
             .rightRearMotorName("brMotor")
-            .leftRearMotorName("flMotor")
-            .leftFrontMotorName("blMotor")
             .leftFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
             .leftRearMotorDirection(DcMotorSimple.Direction.FORWARD)
             .rightFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
             .rightRearMotorDirection(DcMotorSimple.Direction.REVERSE)
-            .xVelocity(47.35551748)
-            .yVelocity(38.10380099);
+            .xVelocity(51.66652688839821)
+            .yVelocity(41.10652143555786);
 
     public static ThreeWheelIMUConstants localizerConstants = new ThreeWheelIMUConstants()
-            .forwardTicksToInches(0.0009259173204549611)
-            .strafeTicksToInches(0.0009467543627825305)
-            .turnTicksToInches(0.001990376215000818)
-            .leftPodY(101/25.4)
+            .forwardTicksToInches( 0.0009161284127254752)
+            .strafeTicksToInches(0.0009416652751526144)
+            .turnTicksToInches(0.0019842565815800695)
+            .leftPodY(101/25.4) //The Third Set
             .rightPodY(-99/25.4)
             .strafePodX(-106/25.4)
             .leftEncoder_HardwareMapName("leftDeadwheel")
@@ -45,7 +68,8 @@ public class Constants {
             .IMU_HardwareMapName("imu")
             .IMU_Orientation(new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.UP, RevHubOrientationOnRobot.UsbFacingDirection.LEFT));
 
-    public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
+
+    public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1.3, 1);
 
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
